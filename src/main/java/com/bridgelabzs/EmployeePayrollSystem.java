c189c653c5f1dbccaf19ca8162b6ee9f2c6644fa
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-06-24 11:22:31 +0530

    feat:established connection between java program and sql

--- src/main/java/com/bridgelabzs/EmployeePayrollSystem.java
+++ src/main/java/com/bridgelabzs/EmployeePayrollSystem.java
@@ -1,9 +1,20 @@
 package com.bridgelabzs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver class loaded successfully");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_services","root","Leela@123");
+		System.out.println("connection established successfully");
+	} catch (ClassNotFoundException | SQLException e) {		
+		e.printStackTrace();
+	}
 
 	}
 
---
+++ src/main/java/com/bridgelabzs/RegisteredDrivers.java
@@ -0,0 +1,16 @@
+package com.bridgelabzs;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class RegisteredDrivers {
+    public static void main(String[] args) {
+
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+        while(drivers.hasMoreElements()) {
+            System.out.println(drivers.nextElement());
+        }
+    }
+}
