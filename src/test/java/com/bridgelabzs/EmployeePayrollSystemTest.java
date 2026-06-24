a97d7bc62df77fb24ce3527cd83c98a0050c1b4f
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-06-24 12:32:42 +0530

    feat:Added junit testing

---
+++ src/test/java/com/bridgelabzs/EmployeePayrollSystemTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabzs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeePayrollSystemTest {
+
+	@Test
+	void printEmployeeDataTest() {
+		EmployeePayrollSystem eps=new EmployeePayrollSystem();
+		List<Employee> emplist=eps.readEmployeeData();
+		assertEquals(5,emplist.size());
+	}
+
+	@Test
+	void updateSqlRecordTest() {
+		EmployeePayrollSystem eps=new EmployeePayrollSystem();
+		int actual=eps.updateAnSqlERecord();
+		assertEquals(1,actual);
+	}
+}
